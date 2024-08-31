cdd2de87d102712074382f21532829da3340f377
Dev Naster <devmaster052@gmail.com>
2024-08-31 18:17:11 +0500

    Removed custom IP address

--- server.js
+++ server.js
@@ -2,15 +2,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./api/routes");
-const { IP_CONFIG } = require("./utils/constants");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/api", routes);
 
-const { ipAddress, port } = IP_CONFIG;
+const port = process.env.PORT || 3000;
 
-app.listen(port, ipAddress, () => {
-  console.log(`Server started on http://${ipAddress}:${port}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
