d25ad067784590de0424eef8b95ba9c65a825d17
Dev Naster <devmaster052@gmail.com>
2024-08-31 23:18:26 +0500

    Rename index file

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const routes = require("./api/routes");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Harmonized backend running...");
+});
+
 app.use(bodyParser.json());
 app.use("/api", routes);
 
