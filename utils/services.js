56c54eb41e0e35f76c3a40b3e2065f758252fcf7
Dev Naster <devmaster052@gmail.com>
2024-09-01 00:04:17 +0500

    Update nodemailer configs

--- utils/services.js
+++ utils/services.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const { auth } = require("../firebase");
 const nodemailer = require("nodemailer");
 const twilio = require("twilio");
 
 const transporter = nodemailer.createTransport({
   service: "Gmail",
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
