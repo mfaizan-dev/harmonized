dff83edeb4825f8079caaa02e82ed2c36bb5766c
Dev Naster <devmaster052@gmail.com>
2024-08-31 23:29:34 +0500

    Rename index file

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// server.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./api/routes");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Harmonized backend running...");
 });
 
-app.use(bodyParser.json());
 app.use("/api", routes);
 
 const port = process.env.PORT || 3000;
